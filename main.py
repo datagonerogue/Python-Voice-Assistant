97525f82a333c379b0f072ea093ee206625eca1d
datagonerogue <botanicaljanuary320@slmail.me>
2024-02-09 06:37:53 +0100

    added speech function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-#test
-def random:
-    added a random command
+import os
+import time
+import playsound
+import speech_recognition as sr 
+from gtts import gTTS
+
+def speak(text):
+    tts = gTTS(text=text, lang="en")
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+
+speak("hello smarth")
