a39118d0d84ff3de4153e29d610fa353d2e69e93
S <129375913+datagonerogue@users.noreply.github.com>
2024-02-10 21:22:33 +0530

    Big Update main.py
    Added conversation logic
    Added saving of speech in directory
    Added directory cleaner

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import os
 import time
+import datetime
+import glob
 import playsound
 import speech_recognition as sr 
 from gtts import gTTS
 
+SPEECH_DIR = "Generated Speech"
+
+if not os.path.exists(SPEECH_DIR):
+    os.makedirs(SPEECH_DIR)
+
+for file in glob.glob(os.path.join(SPEECH_DIR, "*.mp3")):
+    os.remove(file)
+    
 def speak(text):
-    tts = gTTS(text=text, lang="en")
-    filename = "voice.mp3"
+    now = datetime.datetime.now()
+    formatted_time = now.strftime("%H-%M-%S")
+    
+    filename = os.path.join(SPEECH_DIR, f"Speech_{formatted_time}.mp3")
+    tts = gTTS(text=text, tld='us', lang="en", slow=False)
     tts.save(filename)
+    print("rA9: " + text)
     playsound.playsound(filename)
+
     
 def get_audio():
+    print("Listening...")
     r = sr.Recognizer()
     with sr.Microphone() as source:
         audio = r.listen(source)
@@ -18,16 +34,28 @@ def get_audio():
 
         try:
             said = r.recognize_google(audio)
-            print(said)
+            print("You: " + said)
+        except sr.UnknownValueError:
+            speak("Sorry, I am not able to hear you.")
         except Exception as e:
+            speak("An Exception has occured")
             print("Exception: " + str(e))
+            
 
     return said
 
+speak("Hello, I am your virtual assistant. How can I help you?")
 
-audio=get_audio()
-
-if "hello" in audio:
-    speak("Hello, How are you?")
-if "what is your name" in audio:
-    speak("My name is Christopher")
+while True:
+    
+    audio=get_audio()
+    if "hello" in audio:
+        speak("Hello, is there anything i can help you with?")
+    elif "your name" in audio:
+        speak("My name is r A 9")
+    elif audio=="":
+        continue
+    elif audio=="that's it":
+        break
+    else:
+        speak("Sorry, I can't help you with that.")
